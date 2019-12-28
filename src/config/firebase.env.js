b94d6e68aa2ff45bae2b9b0f0b5748a4ea535bf4
Dean Clatworthy <dean.clatworthy@gmail.com>
2019-12-29 01:02:07 +0200

    Always use environmental variables instead

--- src/config/firebase.env.js
+++ src/config/firebase.env.js
@@ -1,18 +1,13 @@
-let firebaseConfig = {}
-if (process.env.FIREBASE_API_KEY) {
-  firebaseConfig = {
-    ...require(`./firebase.default.json`),
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  }
-} else {
-  firebaseConfig = require(`./firebase.json`)
+const firebaseConfig = {
+  ...require(`./firebase.default.json`),
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
 module.exports = firebaseConfig
