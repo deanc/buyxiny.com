38a11622594569f906fb9c3c74aa751b89405f7a
Dean Clatworthy <dean.clatworthy@gmail.com>
2019-12-29 00:10:12 +0200

    Move some build params into optional environmental params to allow netlify to build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
+let firebaseConfig = require(`./src/config/firebase.json`)
+if (process.env.FIREBASE_API_KEY) {
+  firebaseConfig = {
+    ...require(`./src/config/firebase.default.json`),
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  }
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -17,7 +32,7 @@ module.exports = {
       resolve: `@deanc/gatsby-source-firestorer`,
       options: {
         // credential or appConfig
-        config: require(`./src/config/firebase.json`),
+        config: firebaseConfig,
         types: [
           {
             type: `Item`,
