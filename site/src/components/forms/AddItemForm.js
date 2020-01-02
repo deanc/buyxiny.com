c9d09a386ac7a697e8f83d2700f9663526e0ec5a
Dean Clatworthy <dean.clatworthy@gmail.com>
2020-01-02 21:17:37 +0200

    Some tweaks to the form design

--- site/src/components/forms/AddItemForm.js
+++ site/src/components/forms/AddItemForm.js
@@ -1,5 +1,4 @@
 import React from "react"
-import PropTypes from "prop-types"
 
 import { Formik, Form, Field, ErrorMessage, useField } from "formik"
 
@@ -21,7 +20,13 @@ const TextInput = ({ label, ...props }) => {
 const AddItemForm = props => {
   return (
     <Formik
-      initialValues={{ type: "product" }}
+      initialValues={{
+        type: "product",
+        item_name: "",
+        place_name: "",
+        address: "",
+        url: "",
+      }}
       validate={values => {
         const errors = {}
         if (!values.email) {
@@ -34,6 +39,7 @@ const AddItemForm = props => {
         return errors
       }}
       onSubmit={(values, { setSubmitting }) => {
+        console.log(values)
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2))
           setSubmitting(false)
@@ -52,22 +58,88 @@ const AddItemForm = props => {
         /* and other goodies */
       }) => (
         <Form>
-          <div className="form-row">
-            <TextInput name="name" label="Name" />
-          </div>
-          <div className="form-row inline">
-            Type
-            <label>
-              {/* name must match the Formik value key, and must
+          <fieldset>
+            <div className="form-row inline">
+              <div className="label">
+                Please choose whether this is a product or service you are
+                adding:
+              </div>
+              <label>
+                {/* name must match the Formik value key, and must
                 be set on all radio buttons in the group  */}
-              <Field type="radio" name="type" value="product" />
-              Product
-            </label>
-            <label>
-              <Field type="radio" name="type" value="service" />
-              Service
-            </label>
-          </div>
+                <Field type="radio" name="type" value="product" />
+                Product
+              </label>
+              <label>
+                <Field type="radio" name="type" value="service" />
+                Service
+              </label>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <h2>Item information</h2>
+
+            <div className="form-row">
+              <div className="info">
+                <p>Some guidelines:</p>
+                <ul>
+                  <li>
+                    Please enter a fairly descriptive name including the brand
+                    (e.g. 'Typhoo tea' and not just 'Tea', or 'Cadbury Dairy
+                    Milk' not just 'Dairy Milk')
+                  </li>
+                  <li>
+                    If entering a service, be as descriptive as possible also
+                    with the service name.
+                  </li>
+                </ul>
+              </div>
+              <TextInput
+                name="item_name"
+                label="Product/service name (required)"
+                placeholder="Enter the name of the product/service"
+              />
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <h2>Where to buy it?</h2>
+
+            <div className="info">
+              <p>Some guidelines:</p>
+
+              <ul>
+                <li>
+                  If you are entering the name of a chain, enter the city name
+                  as part of the place name. For example don't enter "S-market"
+                  as the place name but rather "S-market Hakaniemi"
+                </li>
+                <li>
+                  For retail stores, please enter an address and website URL if
+                  they have one. For online stores just enter the website URL.
+                </li>
+              </ul>
+            </div>
+            <div className="form-row">
+              <TextInput name="place_name" label="Place name" />
+            </div>
+            <div className="form-row">
+              <TextInput
+                name="url"
+                label="URL"
+                placeholder="https://www.example.com"
+              />
+            </div>
+            <div className="form-row">
+              <TextInput
+                name="address"
+                label="Address (if possible)"
+                placeholder="123 Street, City, Postcode"
+              />
+            </div>
+          </fieldset>
+
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
@@ -76,7 +148,5 @@ const AddItemForm = props => {
     </Formik>
   )
 }
-
-AddItemForm.propTypes = {}
 
 export default AddItemForm
