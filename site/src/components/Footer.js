7a15a207f17ae12b1d67385e9f4c4797cdd75484
Dean Clatworthy <dean.clatworthy@gmail.com>
2020-01-12 22:37:52 +0200

    Added contact link in footer

--- site/src/components/Footer.js
+++ site/src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <footer className="footer container">
       <p>&copy; {currentYear} buyxiny.com</p>
+      <ul>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </footer>
   )
 }
