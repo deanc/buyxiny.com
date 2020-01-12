b66e24bc24ba904e9bc7eec241f399e21a39f07e
Dean Clatworthy <dean.clatworthy@gmail.com>
2020-01-12 22:09:28 +0200

    Added contact form via netlify

---
+++ site/src/pages/contact.js
@@ -0,0 +1,63 @@
+import React from "react"
+
+// import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+import Breadcrumb from "../components/Breadcrumb"
+
+import ShareImage from "../assets/img/fbshare.png"
+
+const ContactPage = () => (
+  <>
+    <SEO
+      title="Contact Us"
+      description={"Get in touch and let us know your thoughts"}
+      image={ShareImage}
+    />
+    <div className="container">
+      <Breadcrumb
+        crumbs={[
+          {
+            label: "Contact Us",
+          },
+        ]}
+      />
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="company"
+        data-netlify-recaptcha="true"
+      >
+        <fieldset>
+          <h2>Contact Us</h2>
+          <div className="form-row nfhp">
+            <label>
+              Don’t fill this out if you're human: <input name="company" />
+            </label>
+          </div>
+          <div className="form-row">
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </div>
+          <div className="form-row">
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </div>
+          <div className="form-row">
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </div>
+          <div className="form-row" data-netlify-recaptcha="true"></div>
+        </fieldset>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  </>
+)
+
+export default ContactPage
